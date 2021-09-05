6d78ac8137b8fa21fe3d3cbd55f66ec547de206b
Rishab Jain <rjrishabh.jain5@gmail.com>
2021-09-06 01:04:45 +0530

    updating after a long time

--- Misc/Main.java
+++ Misc/Main.java
@@ -21,7 +21,7 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {5,2,1};
-        System.out.println(steps(arr));
+        System.out.println(3<<5);
     }
     
 }
